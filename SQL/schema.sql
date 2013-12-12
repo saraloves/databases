cc8709917be63270cbc4e5e8eeddaa8cff523e19
Hack Reactor Students <sarabinns@gmail.com>
2013-12-11 21:04:18 -0800

    Created SQL database for chat messages

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,9 +2,14 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
- /* Describe your table here.*/
+CREATE TABLE `messages` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Users` VARCHAR(20),
+  `Message` VARCHAR(255),
+  `Roomname` VARCHAR(30),
+  PRIMARY KEY  (`ID`)
 );
+
 
 /* You can also create more tables, if you need them... */
 
